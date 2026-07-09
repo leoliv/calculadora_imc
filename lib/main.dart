bb7fd50c6a0ced6ef6d016b710a92437474868bd
Leo Oliveira <leoliv95@gmail.com>
2026-07-09 14:20:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'tela_principal.dart';
+
+void main() => runApp(CalculadoraIMC());
+
+class CalculadoraIMC extends StatelessWidget {
+  const CalculadoraIMC({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const corCinza = Color(0xFF616161);
+    final colorScheme = ColorScheme.dark(
+      primary: corCinza,
+      onPrimary: Colors.white,
+      secondaryContainer: Colors.brown,
+    );
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: colorScheme,
+        scaffoldBackgroundColor: corCinza,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: corCinza,
+          foregroundColor: Colors.white,
+          elevation: 4,
+          shadowColor: Colors.black,
+        ),
+      ),
+      home: TelaPrincipal(),
+    );
+  }
+}
---
+++ lib/tela_principal.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({super.key});
+
+  @override
+  State<TelaPrincipal> createState() =>
+      _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('CALCULADORA IMC')),
+      body: Center(child: Text("Início do app!")),
+    );
+  }
+}
