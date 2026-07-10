f53d2b25ca93f62417c5527a5f77011424747355
Leo Oliveira <leoliv95@gmail.com>
2026-07-10 12:51:59 -0300

    refactor: update layout structure in TelaPrincipal for improved UI

--- lib/tela_principal.dart
+++ lib/tela_principal.dart
@@ -13,7 +13,70 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('CALCULADORA IMC')),
-      body: Center(child: Text("Início do app!")),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CartaoPadrao(
+                    cor: Color(0xff9e9e9e),
+                  ),
+                ),
+                Expanded(
+                  child: CartaoPadrao(
+                    cor: Color(0xff9e9e9e),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CartaoPadrao(
+                    cor: Color(0xff9e9e9e),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CartaoPadrao(
+                    cor: Color(0xff9e9e9e),
+                  ),
+                ),
+                Expanded(
+                  child: CartaoPadrao(
+                    cor: Color(0xff9e9e9e),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CartaoPadrao extends StatelessWidget {
+  // const CartaoPadrao({super.key});
+  const CartaoPadrao({required this.cor, super.key});
+  final Color cor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: cor,
+        borderRadius: BorderRadius.circular(10),
+      ),
     );
   }
 }
